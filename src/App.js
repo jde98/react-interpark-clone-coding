9c41f4419c95042de4299934170b3c7de7b52426
jde98 <ekrjs004@gmail.com>
2022-05-08 16:53:29 +0900

    Route redirect 처리 및 카카오 login route 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientLayout from "./componet/client/content/ClientLayout";
 import ClientPasswordFind from "./componet/client/content/ClientPasswordFind";
 import ClientRegister from "./componet/client/content/ClientRegister";
 import AdminLayout from "./componet/admin/content/AdminLayout";
+import KakaoRedirectHandler from './componet/common/KakaoRedirectHandler';
 
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Routes> 
+
+          <Route path="/"           element={<Navigate replace to="/client"/>} />
           {/* 사용자 페이지 */}
           <Route path="/client/*"   element={<ClientLayout />}></Route> 
           <Route path="/register"   element={<ClientRegister />}></Route> 
@@ -16,6 +19,9 @@ function App() {
 
           {/* 관리자 페이지 */}
           <Route path="/admin/*"    element={<AdminLayout />}></Route>
+
+          {/* 카카오 로그인 */}
+          <Route path="/oauth/callback/kakao"   element={<KakaoRedirectHandler />} />
         </Routes> 
       </BrowserRouter>
     </main>
